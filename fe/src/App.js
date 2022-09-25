0791f258ce6a4c22e8ff8e4e390364ca1e814f6c
Chul Ahn <scahn.x@gmail.com>
2022-09-25 09:00:41 -0400

    style

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,5 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Canvas from './Canvas'
+
 
 import React, { useCallback, useEffect, useState } from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
@@ -58,36 +60,51 @@ function Custom() {
 
     var ipfs_url = `${cid}.ipfs.dweb.link`;
     console.log(ipfs_url);
-    alert(ipfs_url);
     var file_url = `${ipfs_url}/${filename}`
+
+    alert("File successfully uploaded to...")
+    alert(file_url);
+    // document.getSelection("#fileLink").value = file_url;
+
+    console.log(file_url);
     // console.log(process.env.REACT_APP_NFT_PORT_KEY)
 
     axios.post("https://api.nftport.xyz/v0/mints/easy/urls", {
         chain: "polygon",
         name: "3ARTH",
         description: "Chul",
-        file_url: file_url,
-        mint_to_address: polyAddress
+        file_url: "https://bafybeicmqgxjpmkudvij6bxavkkrhar3jnnmztjvlq2ckngn4jue43epgm.ipfs.dweb.link/4js94kg7c4p91.webp",
+        mint_to_address: "0x4Fbf38eFCDeb381F753BAAA22233cA40dF3123Ac"
     }, {
         headers: {
             "Authorization": process.env.REACT_APP_NFT_PORT_KEY,
             "Content-Type": "application/json"
         }
-    }).then(function(response) {
-      console.log(`https://polygonscan.com/tx/${response.data.transaction_hash})`);
+    }, {timeout: 15000}).then(function(response) {
+      var link2 = `https://polygonscan.com/tx/${response.data.transaction_hash})`
+      console.log(link2);
+      alert("View transaction at...");
+      alert(link2);
+      // document.getSelection("#transaction").textContent = link2;
     }).catch(function(error) {
         console.log(error);
     });
 
   }
-  return <div className="mainDiv">Hello, submit music file.
-    <input type="file" name="file" onChange={changeHandler} />
-    <input type="text" id="polyAddress" placeholder="Polygon Address" />
+  return <div className="mainDiv">
+    <h1>3ARTH</h1>
+  Hello, submit a file you want to create a world from.<br/>
+    <input type="file" name="file" onChange={changeHandler} /><br/>
+    Address to send NFT to: <input type="text" id="polyAddress" placeholder="Polygon Address" /><br/>
     <br/>
     <div>
       <button onClick={handleSubmission}>Submit</button>
     </div>
-
+    <div>
+      {/* File Link: <input type="text" id="fileLink"></input><br/>
+      Transaction: <span id="transaction"></span><br/> */}
+    </div>
+    {/* <Canvas/> */}
   </div>;
 }
 
